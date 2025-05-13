034f9b1ac2d81c7af6ed519c9a34373cc46c1a4a
nonody10 <nobodynote10@gmail.com>
2025-05-13 21:46:34 +0530

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -1,9 +1,12 @@
 import os
 
 class Config(object):
-     
+
+    API_ID = os.environ.get("API_ID")
+    if not API_ID:
+        raise ValueError("❌ API_ID is missing in environment variables.")
+    API_ID = int(API_ID)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     MAX_FILE_SIZE = 2194304000
     
